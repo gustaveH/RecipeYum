83a8dd195210fcb4a8a85317e0502f0e699e3199
gustaveH <ghabiman@gmail.com>
2020-06-11 16:30:18 -0400

    Bilt the recipe model

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from "./models/Search"
+import Recipe from "./models/Recipe"
 import * as searchView from "./views/searchView"
 import {elements, renderLoader, clearLoader} from "./views/base"
 
@@ -10,6 +11,9 @@ import {elements, renderLoader, clearLoader} from "./views/base"
  */
 
 const state = {};
+
+
+//** SEARCH controller */
 
 const controlSearch = async () => {
   // 1) Get query from the view
@@ -45,5 +49,10 @@ elements.searchResPages.addEventListener("click", e => {
     searchView.renderResults(state.search.result, goToPage);
   }
 });
+
+//** SEARCH controller */
+const r = new Recipe(35477);
+r.getRecipe();
+console.log(r)
 
 
