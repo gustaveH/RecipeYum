1f465258e0be3d1b2919c995af590fccb70cfd1d
gustaveH <ghabiman@gmail.com>
2020-06-11 15:19:55 -0400

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,49 @@
+import Search from "./models/Search"
+import * as searchView from "./views/searchView"
+import {elements, renderLoader, clearLoader} from "./views/base"
+
+/**Blobal state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - liked recipes
+ */
+
+const state = {};
+
+const controlSearch = async () => {
+  // 1) Get query from the view
+  const query = searchView.getInput();
+
+  if (query){
+    // 2) new search objet and add to state
+    state.search = new Search(query);
+
+    //3) Peepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes)
+
+    // 4) searhc for recipes
+    await state.search.getResults();
+
+    //5) Render results
+    clearLoader()
+    searchView.renderResults(state.search.result);
+  }
+}
+elements.searchForm.addEventListener("submit", e =>{
+  e.preventDefault();
+  controlSearch();
+});
+
+elements.searchResPages.addEventListener("click", e => {
+  const btn = e.target.closest(".btn-inline")
+  if(btn){
+    const goToPage = parseInt(btn.dataset.goto, 10);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+  }
+});
+
+
